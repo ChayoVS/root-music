40c4394df2f1ee1693bb5b0304c3391117cc1e19
rosario VS <paradisekiss-yaoi@hotmail.com>
2020-03-30 23:30:56 -0600

    Updates

---
+++ precache-manifest.2f51292e3c26c31613891883af200a83.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e3df49599740ee3a3fa26a8abdc6359",
+    "url": "/root-music/index.html"
+  },
+  {
+    "revision": "db4bbc77c3ee5c26118a",
+    "url": "/root-music/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "8d4e89c4adb5ad7bfc78",
+    "url": "/root-music/static/js/2.94f359ef.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/root-music/static/js/2.94f359ef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "db4bbc77c3ee5c26118a",
+    "url": "/root-music/static/js/main.2955decd.chunk.js"
+  },
+  {
+    "revision": "ca5c14e8d995ae1027cc",
+    "url": "/root-music/static/js/runtime-main.1bb8b87a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/root-music/static/media/logo.5d5d9eef.svg"
+  }
+]);
